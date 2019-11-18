cc2ca2314bd4b4be24734479f72f41336ad953e1
Knua <dusdn10tkf@korea.ac.kr>
2019-11-17 16:53:21 -0800

    Not Completed (Send Block packet)

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
-#include <pcap.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <linux/types.h>
+#include <errno.h>
 #include <stdint.h>
+#include <string.h>
+#include <pcap.h>
+
+using namespace std;
+
 #define max(a,b) (a > b ? a : b) 
 #define min(a,b) (a < b ? a : b) 
-
-uint16_t ethernet_protocol_type;
-uint8_t ipv4_protocol_id;
 
 uint32_t pow(uint32_t a, uint32_t n){ // return a^n
   uint32_t result = 1;
@@ -16,103 +22,77 @@ uint32_t pow(uint32_t a, uint32_t n){ // return a^n
 }
 
 void usage() {
-  printf("syntax: pcap_test <interface>\n");
-  printf("sample: pcap_test wlan0\n");
+  printf("tcp_block <interface> <host>\n");
+  printf("tcp_block wlan0 test.gilgil.net\n");
 }
 
-// Layer 7 (또는 IPv4, TCP 를 제외한 다른 프로토콜 이용 시 식별되지 않은 이후 32바이트만을 출력)
-void Data_print(const u_char * packet, uint32_t start, uint32_t max_size){
-  printf("\t (Layer 5-7) Data (~ 32 bytes)      :");
-  uint32_t end = start + 32;
-  end = min(end, max_size); // data 의 길이가 32바이트를 넘어갈 경우에도 최대 패킷 크기를 넘지 못하게 함
-  for(int i = start; i < end; i++){
-    if((i - start) % 8 == 0) printf("\n\t\t");
-    printf("0x%02x ", packet[i]);
+char * host_name;
+int host_name_len;
+
+// Layer 7
+bool Data_checking(u_char * packet, uint32_t start, uint32_t end){
+	if(start >= end) return true;
+
+	if(strncmp((const char *)(packet + start), "GET", 3) == 0 ||
+	strncmp((const char *)(packet + start), "POST", 4) == 0 ||
+	strncmp((const char *)(packet + start), "HEAD", 4) == 0 ||
+	strncmp((const char *)(packet + start), "PUT", 3) == 0 ||
+	strncmp((const char *)(packet + start), "DELETE", 6) == 0 ||
+	strncmp((const char *)(packet + start), "OPTIONS", 7) == 0) {
+		// \x0d \x0a -> end of string
+		const char * ptr = strstr((const char *)(packet + start), "Host:");
+		if(ptr != NULL){
+			char save_string[101] = "";
+			for(int i = 6;; i++){
+				if(strncmp((ptr + i), "\x0d", 1) == 0) break;
+				strncat(save_string, (ptr + i), 1);
+			}
+			printf("[Work] Packet Hostname: %s\n", save_string);
+			int cmp_len = strlen(save_string);
+
+			if(strncmp((const char *)save_string, host_name, max(host_name_len, cmp_len)) == 0){
+				printf("[Success] Correct HostName\n");
+				return false;
+			}
+			else printf("[Work] Different HostName\n");
+		}
+		else printf("[Work] No HostName..\n");
+	}
+	return true;
+}
+
+void Data_check(u_char * packet, uint32_t start, uint32_t max_size){
+	uint32_t end = start + 32;
+	end = min(end, max_size);
+  if(!Data_checking(packet, start, end)){ // false means find input hostname
+    block_packet(packet);
   }
-  printf("\n");
+	printf("\n");
+}
+
+void block_packet(u_char * packet){
+  
 }
 
 // Layer 4
-uint32_t TCP_print(const u_char * packet, uint32_t start){
-  uint32_t tcp_start = start;
-
-  uint32_t tcp_src_port_start = tcp_start, tcp_src_port_end = tcp_start + 1;
-  uint16_t tcp_src_port_num = packet[tcp_src_port_start] * 256 + packet[tcp_src_port_end];
-  printf("\t (TCP) Source port                  : %d\n", tcp_src_port_num);
-
-  uint32_t tcp_dst_port_start = tcp_src_port_end + 1, tcp_dst_port_end = tcp_dst_port_start + 1;
-  uint16_t tcp_dst_port_num = packet[tcp_dst_port_start] * 256 + packet[tcp_dst_port_end];
-  printf("\t (TCP) Destination port             : %d\n", tcp_dst_port_num);
-
-  uint32_t tcp_header_length = (packet[tcp_start + 12] & 0xf0) >> 2;
-  printf("\t (TCP) Header Length                : %d bytes\n", tcp_header_length);
-
-  return tcp_start + tcp_header_length;
+uint32_t TCP_check(u_char * packet, uint32_t start){
+	uint32_t tcp_start = start;
+	uint32_t tcp_header_length = (packet[tcp_start + 12] & 0xf0) >> 2;
+	return tcp_start + tcp_header_length;
 }
 
 // Layer 3
-uint32_t IPv4_print(const u_char * packet, uint32_t start){
-  uint32_t ipv4_start = start;
-  
-  uint32_t ipv4_header_length = (packet[ipv4_start] & 0x0f) * 4;
-  printf("\t (IPv4) Header Length               : %d bytes\n", ipv4_header_length);
-
-  uint32_t ipv4_protocol_ID = packet[ipv4_start + 9];
-  ipv4_protocol_id = ipv4_protocol_ID;
-  printf("\t (IPv4) Protocol ID                 : %d\n", ipv4_protocol_ID);
-
-  printf("\t (IPv4) IP source address           : ");
-  uint32_t ipv4_src_addr_start = ipv4_start + 12;
-  uint32_t ipv4_src_addr_end = ipv4_src_addr_start + 4;
-  for(int i = ipv4_src_addr_start; i < ipv4_src_addr_end; i++){
-    printf("%d", packet[i]);
-    if(i == ipv4_src_addr_end - 1) break;
-    printf(".");
-  }
-  printf("\n");
-
-  printf("\t (IPv4) IP destination address      : ");
-  uint32_t ipv4_dst_addr_start = ipv4_start + 16;
-  uint32_t ipv4_dst_addr_end = ipv4_dst_addr_start + 4;
-  for(int i = ipv4_dst_addr_start; i < ipv4_dst_addr_end; i++){
-    printf("%d", packet[i]);
-    if(i == ipv4_dst_addr_end - 1) break;
-    printf(".");
-  }
-  printf("\n");
-
-  return ipv4_start + ipv4_header_length;
-}
- 
-// Layer 2 (only Ethernet checking)
-uint32_t Ethernet_print(const u_char * packet){
-  printf("\t (Ethernet) MAC source address      : ");
-    for(int i = 6; i < 12; i++){
-      printf("%02x", packet[i]);
-      if(i == 11) break; 
-      printf(":");
-    }
-    printf("\n");
-    printf("\t (Ethernet) MAC destination address : ");
-    for(int i = 0; i < 6; i++){
-      printf("%02x", packet[i]);
-      if(i == 5) break;
-      printf(":");
-    }
-    printf("\n");
-    uint16_t protocol_type = 0;
-    printf("\t (Ethernet) Protocol Type           : ");
-    for(int i = 12; i < 14; i++){
-      protocol_type += packet[i] * pow(256, 13-i);
-    }
-    printf("0x%04x \n", protocol_type);
-    ethernet_protocol_type = protocol_type;
-
-    return 14; // ethernet header end
+uint8_t ipv4_protocol_id;
+uint32_t IPv4_check(u_char * packet, uint32_t start){
+	uint32_t ipv4_start = start;
+	uint32_t ipv4_header_length = (packet[ipv4_start] & 0x0f) * 4;
+	ipv4_protocol_id = packet[ipv4_start + 9];
+	return ipv4_start + ipv4_header_length;
 }
 
-int main(int argc, char* argv[]) {
-  if (argc != 2) {
+int main(int argc, char* argv[]) 
+{
+  if (argc != 3) {
     usage();
     return -1;
   }
@@ -125,31 +105,25 @@ int main(int argc, char* argv[]) {
     return -1;
   }
 
-  int packetNum = 0;
+	host_name = argv[2];
+	host_name_len = strlen(host_name);
+
   while (true) {
     struct pcap_pkthdr* header;
-    const u_char* packet;
+    u_char* packet;
     int res = pcap_next_ex(handle, &header, &packet);
     if (res == 0) continue;
     if (res == -1 || res == -2) break;
     
-    printf("[Packet %d]\n", ++packetNum);
-    printf("\t Packet size                        : %u bytes\n", header->caplen);
-    uint32_t ethernet_header_end = Ethernet_print(packet);
-
-    // Ethernet -> IPv4 -> TCP -> (Data)
     uint32_t ipv4_header_end = 0;
-    uint32_t tcp_header_end = 0;
+	  uint32_t tcp_header_end = 0;
 
-    if(ethernet_protocol_type == 0x0800){ // IPv4
-      ipv4_header_end = IPv4_print(packet, ethernet_header_end);
-    }
+    ipv4_header_end = IPv4_check(packet, 0);
     if(ipv4_protocol_id == 0x6){ // IPv4 -> TCP
-      tcp_header_end = TCP_print(packet, ipv4_header_end);
+    	tcp_header_end = TCP_check(packet, ipv4_header_end);
     }
-    Data_print(packet, max(max(ethernet_header_end, ipv4_header_end), tcp_header_end), header->caplen);
-    // 만약 2계층에서는 ethernet protocol 을 이용하였으나 상위 계층에서 ipv4, tcp protocol 을 이용하지 않은 경우, 이후 (최대) 32bytes 만을 출력합니다.
-    printf("\n\n");
+    Data_check(packet, max(ipv4_header_end, tcp_header_end), header->caplen);
+    printf("\n");
   }
 
   pcap_close(handle);
